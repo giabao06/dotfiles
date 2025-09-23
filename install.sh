4852e6a4581f0752cce9641ef2b107fe80c1cb6b
Nguyễn Phạm Gia Bảo <hello@giabao06.xyz>
2025-09-23 13:09:56 +0700

    alacritty joins the game

--- install.sh
+++ install.sh
@@ -4,3 +4,5 @@ echo "creating symlink for hyprland configs"
 ln -s $(pwd)/.config/hypr $HOME/.config/hypr
 echo "creating symlink for waybar config"
 ln -s $(pwd)/.config/waybar $HOME/.config/waybar
+echo "creating symlink for alacritty config"
+ln -s $(pwd)/.config/alacritty $HOME/.config/alacritty
